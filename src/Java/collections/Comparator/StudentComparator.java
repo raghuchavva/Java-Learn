55ce66da5c8b4fae6a6d35080c34c89380c3a7c8
Raghunatha Reddy <raghu.chavva2023@gmail.com>
2026-06-06 07:54:36 +0530

    Revert "Comparator"
    This reverts commit 12137e3ccb05d20da8a915c7d414a99f18fd2d16.

--- src/Java/collections/Comparator/StudentComparator.java
+++ src/Java/collections/Comparator/StudentComparator.java
@@ -1,10 +1,4 @@
 package Java.collections.Comparator;
 
-import java.util.Comparator;
-
-public class StudentComparator implements Comparator<Student> {
-
-    public int compare(Student e1,Student e2){
-        return e1.getName().compareTo(e2.getName());
-    }
+public class StudentComparator {
 }
