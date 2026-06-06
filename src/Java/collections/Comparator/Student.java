12137e3ccb05d20da8a915c7d414a99f18fd2d16
Raghunatha Reddy <raghu.chavva2023@gmail.com>
2026-06-06 07:28:29 +0530

    Comparator

--- src/Java/collections/Comparator/Student.java
+++ src/Java/collections/Comparator/Student.java
@@ -1,4 +1,36 @@
 package Java.collections.Comparator;
 
-public class Student {
+import java.util.Comparator;
+
+public class Student{
+    private String name;
+    private int id;
+    Student(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
